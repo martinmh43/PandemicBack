c6e1ac1c031a4a49002b1adfa55b729c1b262bb4
Martín <martinmh43@educastur.es>
2024-12-07 10:35:34 +0100

    register actu mano

--- register.php
+++ register.php
@@ -1,9 +1,12 @@
 <?php
 
-$nombre = $_POST['nombre'];
-$password = $_POST['password'];
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers:*");
+header("Content-Type: aplication/json");
 
+$json = file_get_contents("php://login");
 
+$usuario = json_decode($json);
 
 $servername = "localhost";
 $username = "root";
@@ -13,15 +16,24 @@ $dbname = "pandemic";
 $conn = new mysqli($servername, $username, $dbpass, $dbname);
 
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+   echo "Error en la conexion manito";
+   echo json_encode([
+    "status"=>"failed",
+    "message"=>"Error al realizar la conexion"
+    ]);
 }
 
-$insertarAlumno = "INSERT INTO usuarios (nombre, contraseña, foto, email) VALUES ('$nombre', '$password', null, null)";
+$insertarAlumno = "INSERT INTO usuarios (nombre, contraseña, foto, email) VALUES ('$usuario->$nombre', '$usuario->$password', null, null)";
 
 if ($conn->query($insertarAlumno) === TRUE) {
-    echo "Se insertó correctamente al usuario " . $nombre;
-} else {
-    echo "Error al insertar al usuario: " . $conn->error;
+    echo json_encode([
+        "status"=>"success",
+        "message"=>"El user $usuario->$nombre se ha registrado PI:OLA"
+    ]);
+} else {echo json_encode([
+    "status"=>"failure",
+    "message"=>"Error al insertar al usuario: " . $usuario->$nombre
+    ]);
 }
 
 
